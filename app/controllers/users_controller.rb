65befbd82df6496dd903edbc6d3d88b82dd135ad
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-02-25 18:44:02 +0900

    ユーザーの一覧・詳細画面を表示できるようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   def index
+    @users = User.order(:id).page(params[:user])
   end
 
   def show
-  end
-
-  def edit
+    @user = User.find(params[:id])
   end
 end
