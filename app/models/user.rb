20050035d71b7df10360c392432438b170344161
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-01 01:51:50 +0900

    User, Followモデルに多対多の関連付けをする

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :follower, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followee, class_name: 'Follow', foreign_key: 'followee_id', dependent: :destroy
+  has_many :followings, through: :follower, source: :followee
+  has_many :followers, through: :followee, source: :follower
 end
