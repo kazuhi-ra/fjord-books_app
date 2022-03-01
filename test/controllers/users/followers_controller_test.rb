d0cf3c4f6212f4edb5e8b82819e14251db37c8cf
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-01 20:08:03 +0900

    rails g controller users::followers index

---
+++ test/controllers/users/followers_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class Users::FollowersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get users_followers_index_url
+    assert_response :success
+  end
+end
