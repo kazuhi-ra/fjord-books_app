ed2ea7d4a662258923011d37691dad7da51c69fa
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-01 23:51:33 +0900

    RuboCopがinverse_ofを指定するよう教えてくれたので修正する

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  has_many :follower, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy
-  has_many :followee, class_name: 'Follow', foreign_key: 'followee_id', dependent: :destroy
+  has_many :follower, class_name: 'Follow', foreign_key: 'follower_id', inverse_of: 'follower', dependent: :destroy
+  has_many :followee, class_name: 'Follow', foreign_key: 'followee_id', inverse_of: 'followee', dependent: :destroy
   has_many :followings, through: :follower, source: :followee
   has_many :followers, through: :followee, source: :follower
 
