2d0dce67ef16e869270501c11959819c0d1ff63b
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-01 20:07:17 +0900

    users/:user_id/followings で画面が表示できるようにルーティングする

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  namespace :users do
-    get 'followings/index'
-  end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
-  resources :users, only: %i(index show)
+  resources :users, only: %i(index show) do
+    resources :followings, only: [:index], module: 'users'
+  end
   post 'follows/:id' => 'follows#create', as: :follow
   delete 'follows/:id' => 'follows#destroy', as: :unfollow
 end
