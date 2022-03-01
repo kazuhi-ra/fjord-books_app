471a449f9eb5192e259c054e42393a32ddae1040
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-01 14:40:45 +0900

    ユーザーをフォロー・フォロー解除するための機能を作成する

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
   has_many :followee, class_name: 'Follow', foreign_key: 'followee_id', dependent: :destroy
   has_many :followings, through: :follower, source: :followee
   has_many :followers, through: :followee, source: :follower
+
+  def follow(user_id)
+    follower.find_or_create_by(followee_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followee_id: user_id)&.destroy
+  end
 end
