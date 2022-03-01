f53b21d535743bfa1ba9b43cb951f9cfde83ce1c
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-01 22:08:17 +0900

    フォロワー一覧画面を作成する

--- app/controllers/users/followers_controller.rb
+++ app/controllers/users/followers_controller.rb
@@ -1,4 +1,8 @@
 class Users::FollowersController < ApplicationController
   def index
+    def index
+      @user = User.find(params[:user_id])
+      @followers = @user.followers.order(:id).page(params[:page])
+    end
   end
 end
