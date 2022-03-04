a2d27ec7a771a83191095fc5ccf3c7c179b615a5
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-04 20:22:16 +0900

    日報にコメントをつけられるようにする

---
+++ app/controllers/reports/comments_controller.rb
@@ -0,0 +1,22 @@
+class Reports::CommentsController < ApplicationController
+  def create
+    report = Report.find(report_id)
+    new_comment = report.comments.new(content: comment_params[:content],user: current_user)
+
+    if new_comment.save
+      redirect_to report, notice: t('controllers.common.notice_comment_created')
+    else
+      redirect_to report, alert: new_comment.errors.full_messages
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def report_id
+    params[:report_id]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
       resources :followers, only: [:index]
     end
   end
+  resources :reports, only: %i[show] do
+    scope module: :reports do
+      resources :comments, only: %i[create]
+    end
+  end
 end
