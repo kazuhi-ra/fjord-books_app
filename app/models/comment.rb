ab5dd4eb84c7100f854f7de55c0a27149da5419c
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-04 20:49:45 +0900

    本の詳細ページにコメントを表示できるようにする

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
 
+  has_many :comments, as: :commentable
+
   validates :content, presence: true
 end
