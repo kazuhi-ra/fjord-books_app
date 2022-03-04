89dcf92562504c9f8b080c3c1c5cfe9889bd9783
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-04 21:51:14 +0900

    誤ってcomment自身がcommentを持つ形になってしまっていたのを修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,5 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
 
-  has_many :comments, as: :commentable
-
   validates :content, presence: true
 end
