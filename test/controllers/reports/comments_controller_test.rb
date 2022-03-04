b1bdf681dee7c0639cbcd6e69c6dedd9bea05f82
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-04 21:34:43 +0900

    使用していない自動生成ファイルを削除する

--- test/controllers/reports/comments_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Reports::CommentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get reports_comments_create_url
-    assert_response :success
-  end
-end
