ea8f3ff01049ae8ca4ea65ad61d11110305b6b92
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-10 19:05:34 +0900

    has_manyにふさわしいように複数形の名前にする

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,20 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  has_many :follower, class_name: 'Follow', foreign_key: 'follower_id', inverse_of: 'follower', dependent: :destroy
-  has_many :followee, class_name: 'Follow', foreign_key: 'followee_id', inverse_of: 'followee', dependent: :destroy
-  has_many :followings, through: :follower, source: :followee
-  has_many :followers, through: :followee, source: :follower
+  has_many :follower_relations, class_name: 'Follow', foreign_key: 'follower_id', inverse_of: 'follower', dependent: :destroy
+  has_many :followee_relations, class_name: 'Follow', foreign_key: 'followee_id', inverse_of: 'followee', dependent: :destroy
+  has_many :followings, through: :follower_relations, source: :followee
+  has_many :followers, through: :followee_relations, source: :follower
 
   def follow(user)
-    follower.find_or_create_by(followee_id: user.id)
+    follower_relations.find_or_create_by(followee_id: user.id)
   end
 
   def unfollow(user)
-    follower.find_by(followee_id: user.id)&.destroy
+    follower_relations.find_by(followee_id: user.id)&.destroy
   end
 
   def following?(user)
-    follower.where(followee_id: user.id).exists?
+    follower_relations.where(followee_id: user.id).exists?
   end
 end
