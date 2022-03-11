a07488cd11d4ca316755e88906f3564203dcf0b4
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-11 18:08:22 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_11_082627) do
+ActiveRecord::Schema.define(version: 2022_03_11_090156) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(version: 2022_03_11_082627) do
   end
 
   create_table "reports", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
