b6735f3f07c721013a42664eaf23b92b6a481310
kazuhi-ra <kazuhi-ra@pepabo.com>
2022-03-11 12:14:37 +0900

    MiniMagickのオプションを見直す

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,6 @@
 
 module UsersHelper
   def avatar(user:, size:)    
-    image_tag user.avatar.variant(resize_to_limit: [size, size], format: :jpeg, sampling_factor: "4:2:0", strip: true, interlace: "JPEG", colorspace: "sRGB", quality: 80), size: "#{size}x#{size}"
+    image_tag user.avatar.variant(resize_to_limit: [size, size], strip: true)
   end
 end
